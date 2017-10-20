477ec0af9fec599778db8ea602e18668f8b8194e
Tedd Kourkounakis <teddk@rogers.com>
2017-10-20 17:20:15 -0400

    REF TB-10: moved piece constants from common.h to board.h

--- board.h
+++ board.h
@@ -1,4 +1,20 @@
 #pragma once
+
+// chess piece constants
+#define B_KING 'k'
+#define B_QUEEN 'q'
+#define B_BISHOP 'b'
+#define B_ROOK 'r'
+#define B_KNIGHT 'n'
+#define B_PAWN 'p'
+#define W_KING 'K'
+#define W_QUEEN 'Q'
+#define W_BISHOP 'B'
+#define W_ROOK 'R'
+#define W_KNIGHT 'N'
+#define W_PAWN 'P'
+#define EMPTY ' '
+
 class board
 {
 public:
