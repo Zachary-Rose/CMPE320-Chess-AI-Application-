f2a876873445acdd968d3d8c183a3fdfe0c630b6
Mark Asselin <mark.asselin95@gmail.com>
2017-10-26 00:08:21 -0400

    ENH TB-42: Adding general structure of Ai class

--- ai.cpp
+++ ai.cpp
@@ -1,12 +1,84 @@
 #include "ai.h"
+#include "common.h"
 
-
-
-ai::ai()
+//--------------------------------------------------------------------------------
+Ai::Ai()
 {
+  LOG_TRACE("Ai::Ai()");
 }
 
-
-ai::~ai()
+//--------------------------------------------------------------------------------
+Ai::~Ai()
 {
+  LOG_TRACE("Ai::~Ai()");
+}
+
+//--------------------------------------------------------------------------------
+Move Ai::GetBestMove(PLAYER WhosTurnIsIt)
+{
+  LOG_TRACE("Move Ai::GetBestMove(PLAYER WhosTurnIsIt)");
+  return Move();
+}
+
+//--------------------------------------------------------------------------------
+std::vector<Move> Ai::GetLegalPieceMoves(const BoardPosition& bp)
+{
+  LOG_TRACE("std::vector<Move> Ai::GetLegalPieceMoves(const BoardPosition& bp)");
+  return std::vector<Move>();
+}
+
+//--------------------------------------------------------------------------------
+Move Ai::ExecuteMove(PLAYER WhosTurnIsIt, Move move)
+{
+  LOG_TRACE("Move Ai::ExecuteMove(PLAYER WhosTurnIsIt, Move move)");
+  return Move();
+}
+
+//--------------------------------------------------------------------------------
+std::vector<Move> Ai::GenerateRookMoves()
+{
+  LOG_TRACE("std::vector<Move> Ai::GenerateRookMoves()");
+  return std::vector<Move>();
+}
+
+//--------------------------------------------------------------------------------
+std::vector<Move> Ai::GenerateKnightMoves()
+{
+  LOG_TRACE("std::vector<Move> Ai::GenerateKnightMoves()");
+  return std::vector<Move>();
+}
+
+//--------------------------------------------------------------------------------
+std::vector<Move> Ai::GenerateBishopMoves()
+{
+  LOG_TRACE("std::vector<Move> Ai::GenerateBishopMoves()");
+  return std::vector<Move>();
+}
+
+//--------------------------------------------------------------------------------
+std::vector<Move> Ai::GenerateKingMoves()
+{
+  LOG_TRACE("std::vector<Move> Ai::GenerateKingMoves()");
+  return std::vector<Move>();
+}
+
+//--------------------------------------------------------------------------------
+std::vector<Move> Ai::GenerateQueenMoves()
+{
+  LOG_TRACE("std::vector<Move> Ai::GenerateQueensMoves()");
+  return std::vector<Move>();
+}
+
+//--------------------------------------------------------------------------------
+std::vector<Move> Ai::GeneratePawnMoves()
+{
+  LOG_TRACE("std::vector<Move> Ai::GeneratePawnMoves()");
+  return std::vector<Move>();
+}
+
+//--------------------------------------------------------------------------------
+std::vector<Move> Ai::GenerateAllPossibleMoves()
+{
+  LOG_TRACE("std::vector<Move> Ai::GenerateAllPossibleMoves()");
+  return std::vector<Move>();
 }
--- ai.h
+++ ai.h
@@ -1,8 +1,57 @@
 #pragma once
-class ai
+
+// local includes
+#include "board.h"
+#include "move.h"
+
+// system includes
+#include <vector>
+
+enum PLAYER
+{
+  WHITE = 0,
+  BLACK = 1
+};
+
+class Ai
 {
 public:
-  ai();
-  ~ai();
+  Ai();
+  ~Ai();
+
+  // gets best move for provided player
+  Move GetBestMove(PLAYER WhosTurnIsIt);
+
+  // generates list of legal piece moves for piece at bp
+  std::vector<Move> GetLegalPieceMoves(const BoardPosition& bp);
+
+  // executes move (either made by player via GUI, or by computer via AI)
+  Move ExecuteMove(PLAYER WhosTurnIsIt, Move move);
+
+private:
+  //member variables
+  Board GameBoard;
+  PLAYER WhosTurnIsIt; // who's turn is it right now?
+
+  // generates list of all legal rook moves
+  std::vector<Move> GenerateRookMoves();
+
+  // generates list of all legal knight moves
+  std::vector<Move> GenerateKnightMoves();
+
+  // generates list of all legal bishop moves
+  std::vector<Move> GenerateBishopMoves();
+
+  // generates list of all legal king moves
+  std::vector<Move> GenerateKingMoves();
+
+  // generates list of all legal queen moves
+  std::vector<Move> GenerateQueenMoves();
+
+  // generates list of all legal pawn moves
+  std::vector<Move> GeneratePawnMoves();
+
+  // generates list of all possible legal moves
+  std::vector<Move> GenerateAllPossibleMoves();
 };
 
