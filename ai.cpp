90638c0090a6fecd1dde8d7abbddc94e25dde48a
cstaffieri <cordelia.staffieri@gmail.com>
2017-11-13 16:23:59 -0500

    BUG Fixes to King, Queen, and Bishop mechanics

--- ai.cpp
+++ ai.cpp
@@ -150,7 +150,7 @@ bool Ai::isKingSafe(bool blackOrWhite, Move move) {
 		outOfBound = false;
 	}
 
-	//check if opposite knight can attack (stolen from tedd's code)
+	//check if opposite knight can attack (stolen from tedd's code and repurposed)
 	std::vector<std::vector<int>> pos{
 		{ 1, 2 },
 		{ 2, 1 },
@@ -264,7 +264,7 @@ std::vector<Move> Ai::GenerateBishopMoves(int Start_i, int Start_j)
 			if (Start_i + temp * j < 0 || Start_i + temp * j > 7
 				|| Start_j + temp * k < 0 || Start_j + temp * k > 7)
 			{
-				outOfBound = true;
+				continue;
 			}
 
 			while (!outOfBound && GameBoard.GetPieceByPosition(Start_i + temp * j, Start_j + temp * k) == EMPTY)
@@ -338,19 +338,20 @@ std::vector<Move> Ai::GenerateKingMoves(int Start_i, int Start_j)
 		for (int k = -1; k <= 1; k++)
 		{
 			if (Start_i + j < 0 || Start_i + j > 7
-			 || Start_j + k < 0 || Start_j + k > 7)
+				|| Start_j + k < 0 || Start_j + k > 7)
 			{
-				pieceTaken = GameBoard.GetPieceByPosition(Start_i + 1 * j, Start_j + 1 * k);
-				if (pieceTaken == ' '
-					|| (blackOrWhite && islower(pieceTaken))
-					|| (!blackOrWhite && isupper(pieceTaken)))
-				{
-					//again a king safe method should be added here, wrapping the below line in an if statement
-					KingMoves.push_back(Move(Start_i, Start_j, Start_i + 1 * j, Start_j + 1 * k, pieceTaken));
-				}
+				continue;
+			}
+
+			pieceTaken = GameBoard.GetPieceByPosition(Start_i + 1 * j, Start_j + 1 * k);
+			if (pieceTaken == ' '
+				|| (blackOrWhite && islower(pieceTaken))
+				|| (!blackOrWhite && isupper(pieceTaken)))
+			{
+				//again a king safe method should be added here, wrapping the below line in an if statement
+				KingMoves.push_back(Move(Start_i, Start_j, Start_i + 1 * j, Start_j + 1 * k, pieceTaken));
 			}
 		}
-
 		return std::vector<Move>();
 	}
 }
@@ -376,7 +377,7 @@ std::vector<Move> Ai::GenerateQueenMoves(int Start_i, int Start_j)
 			if (Start_i + temp * j < 0 || Start_i + temp * j > 7
 				|| Start_j + temp * k < 0 || Start_j + temp * k > 7)
 			{
-				outOfBound = true;
+				continue;
 			}
 
 
